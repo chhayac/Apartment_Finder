603e7c238d144c2f777cf11f0efa61ecda9f2e30
Chhaya Choudhary <er.chhayachoudhary@gmail.com>
2017-06-13 23:34:52 -0700

    Update apartment_finder.py

--- apartment_finder.py
+++ apartment_finder.py
@@ -1,20 +1,21 @@
 import config_parser
-
+import scrapper
+import json
 
 class Apartment_Finder():
     def __init__(self):
         self.config_parser = config_parser.Configuration_Parser()
+        self.scrapper = scrapper.Scrapper(self.config_parser)
     
-    def get_data(self, source_address):
-        maps_url = self.config_parser.get_maps_url(source_address)
-        print(maps_url)
-
+    def print_data(self):
+        apartments = self.scrapper.get_apartments_info()
+        for apartment in apartments:
+            print(apartment)
 
 def main():
     ap = Apartment_Finder()
     print()
-    source_address = 'Times Square, Manhattan, NY 10036'
-    ap.get_data(source_address)
+    ap.print_data()
     print()
 
 if __name__ == '__main__':
